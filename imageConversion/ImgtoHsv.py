f8ed5a3d3b6803cb1a12a8074cbf2ca9b329f7eb
Arnav0405 <arnavwaghdhare@gmail.com>
2024-09-11 00:16:22 +0530

    Revert "Changed from OpenCV to Pillow"
    This reverts commit 63fff111b36d7a27f88acfb7f76773c851a1a1e3.

--- imageConversion/ImgtoHsv.py
+++ imageConversion/ImgtoHsv.py
@@ -1,66 +1,41 @@
-from PIL import Image, ImageOps
+import cv2, time
 import matplotlib.pyplot as plt
 import numpy as np
 
-image = Image.open('./imageConversion/eqImg.png')
-imArr = np.array(image.convert('RGB'))
-#image.show()
 
 def img2SmallImg(img, H_SIZE, W_SIZE):
-    listOfImages = []
-    for ih in range(H_SIZE):
-        for iw in range(W_SIZE):
-            x = width / W_SIZE * iw
-            y = height / H_SIZE * ih
-            h = (height / H_SIZE)
-            w = (width / W_SIZE)
-            #print(x,y,h,w)  
-
-            img2 = img[int(y):int(y+h), int(x):int(x+w)]
-            listOfImages.append(img2)
-
-    return np.array(listOfImages)
-
-
-def smallImg2AvgArr(arr):
-   lst2npArray = np.array(arr)
-   lst = lst2npArray.transpose((3, 0, 1, 2))
+   listOfImages = []
+   for ih in range(H_SIZE ):
+     for iw in range(W_SIZE ):
+     
+        x = width/W_SIZE * iw 
+        y = height/H_SIZE * ih
+        h = (height / H_SIZE)
+        w = (width / W_SIZE )
+        #print(x,y,h,w)
+        img2 = img[int(y):int(y+h), int(x):int(x+w)]
+        listOfImages.append(img2)
    
-   avg = lst.mean(axis= 0, dtype= int)
-
-   return lst[1]
-
-def convert2grey(img):
-    img = ImageOps.grayscale(img)
-    imgArr = img.convert('L')
-    return np.array(imgArr)
+   return listofImages
 
 
-print(imArr)
-height, width, channels = imArr.shape
-#print(image.shape[0] % 11, image.shape[1] % 22)
+
+image = cv2.imread('./eqImg.png')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+print(type(image))
+height, width, channels = image.shape
+
+print(image.shape)
+print(image.shape[0] % 11, image.shape[1] % 22)
+print(image[[0], [0]])
 
 W_SIZE = 22
 H_SIZE = 11
 count = 0
 
 
-lst = img2SmallImg(imArr, H_SIZE, W_SIZE)
-print(lst.shape)
-print(lst[0][0][0])   #This is one pixel, the top right most pixel
 
-plt.imshow(lst[0][0])      #This is one column of pixels in the right most side??
-plt.show()
-#print(smallImg2AvgArr(lst))
+print(img2SmallImg(image, H_SIZE, W_SIZE))
+
 #plt.show()
 
-
-#convert to greyscale then map that to sound parts
-greyArr = convert2grey(image)
-
-plt.imshow(greyArr, cmap="gray")
-plt.show()
-#cv2.waitKey(5000)  
-
-# Window shown waits for any key pressing event
-#cv2.destroyAllWindows()
--- imageConversion/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-cv2.imshow('Grayscale', image)
-cv2.waitKey(0)  
-
-# Window shown waits for any key pressing event
-cv2.destroyAllWindows()
